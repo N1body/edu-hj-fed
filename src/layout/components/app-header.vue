26c48cb36c91f9ddac5e3f3cdb48bfc87522c0d2
N1body <hjing0524@gmail.com>
2022-05-04 14:38:29 +0800

    设置面包屑导航

--- src/layout/components/app-header.vue
+++ src/layout/components/app-header.vue
@@ -2,26 +2,73 @@
   <div class="header">
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>课程管理</el-breadcrumb-item>
+      <el-breadcrumb-item>{{routeRecord}}</el-breadcrumb-item>
     </el-breadcrumb>
     <el-dropdown @command="handleCommand">
       <span class="el-dropdown-link">
-        <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+        <el-avatar :src="userInfo.portrait ||  require('../../assets/default-avatar.png')"></el-avatar>
       </span>
       <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item command="phone">15579919192</el-dropdown-item>
-        <el-dropdown-item command="exit">退出</el-dropdown-item>
+        <el-dropdown-item command="phone">{{userInfo.userName}}</el-dropdown-item>
+        <el-dropdown-item divided command="exit">退出</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
   </div>
 </template>
 
 <script>
+import { getUerInfo } from "@/services/user"
 export default {
   name: 'AppHeader',
+  data () {
+    return {
+      userInfo: {}
+    }
+  },
+  created () {
+    this.loadUserInfo()
+  },
   methods: {
     handleCommand(command) {
-      this.$message('click on item ' + command);
+      if (command === 'exit') {
+          this.$confirm('确认退出吗?', '退出提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          // 清楚用户的登录状态
+          this.$store.commit('setUser', null)
+
+          // 跳转到登录页面
+          this.$router.push({
+            name: 'login'
+          })
+
+          this.$message({ // 确认执行这里
+            type: 'success',
+            message: '退出成功!'
+          })
+        }).catch(() => {
+          this.$message({ // 取消执行这里
+            type: 'info',
+            message: '已取消退出'
+          })
+        })
+      }
+    },
+    async loadUserInfo () {
+      const { data } = await getUerInfo()
+      if (data.state !== 1) {
+        this.$message.error(data.message)
+      }
+      else {
+        this.userInfo = data.content
+      }
+    }
+  },
+  computed: {
+    routeRecord () {
+      return this.$route.matched[1].meta.title
     }
   }
 }
