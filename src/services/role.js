2662c7e55a36cf50752e8e5d6efffce9e3fc1b16
N1body <hjing0524@gmail.com>
2022-05-06 14:46:01 +0800

    角色管理相关请求接口

---
+++ src/services/role.js
@@ -0,0 +1,27 @@
+import request from '@/utils/request'
+
+// 获取角色列表
+export const getRolePages = data => {
+  return request({
+    method: 'POST',
+    url: '/boss/role/getRolePages',
+    data
+  })
+}
+
+// 根据 id 删除角色
+export const deleteRole = id => {
+  return request({
+    method: 'DELETE',
+    url: `/boss/role/${id}`
+  })
+}
+
+// 更新角色列表信息
+export const createOrUpdate = data => {
+  return request({
+    method: 'POST',
+    url: '/boss/role/saveOrUpdate',
+    data
+  })
+}
