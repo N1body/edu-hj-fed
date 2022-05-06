f4d2a4828a84ae63e3a3cf2940f2be5b5917869b
N1body <hjing0524@gmail.com>
2022-05-06 18:32:10 +0800

    封装菜单管理相关的请求

--- src/services/menu.js
+++ src/services/menu.js
@@ -18,3 +18,35 @@ export const getRoleMenus = roleId => {
     }
   })
 }
+
+export const createOrUpdateMenu = data => {
+  return request({
+    method: 'POST',
+    url: '/boss/menu/saveOrUpdate',
+    data
+  })
+}
+
+export const getEditMenuInfo = id => {
+  return request({
+    method: 'GET',
+    url: '/boss/menu/getEditMenuInfo',
+    params: {
+      id
+    }
+  })
+}
+
+export const getAllMenus = () => {
+  return request({
+    method: 'GET',
+    url: '/boss/menu/getAll'
+  })
+}
+
+export const deleteMenu = id => {
+  return request({
+    method: 'DELETE',
+    url: `/boss/menu/${id}`
+  })
+}
