601ab966c1650e943635e64b687f7fa461c94b2f
N1body <hjing0524@gmail.com>
2022-05-06 18:46:11 +0800

    编辑菜单功能

---
+++ src/views/menu/Edit.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <CreateOrEdit title="编辑菜单" :menuId="menuId"/>
+  </div>
+</template>
+
+<script>
+import CreateOrEdit from './components/CreateOrEdit.vue'
+export default {
+  name: 'EditMenu',
+  props: ['menuId'],
+  components: {
+    CreateOrEdit
+  }
+}
+</script>
+
+<style>
+
+</style>
