bafbb34ef7c0e3d87ab1841f0a8546c5a059c40d
N1body <hjing0524@gmail.com>
2022-05-06 18:45:58 +0800

    添加菜单功能

---
+++ src/views/menu/Create.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <CreateOrEdit title="添加菜单"/>
+  </div>
+</template>
+
+<script>
+import CreateOrEdit from "./components/CreateOrEdit.vue";
+export default {
+  name: 'CreateMenu',
+  components: {
+    CreateOrEdit
+  }
+}
+</script>
+
+<style>
+
+</style>
