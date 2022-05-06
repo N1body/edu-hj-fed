220623f8c8484df491fc4d902ee32faf10d2d455
N1body <hjing0524@gmail.com>
2022-05-06 20:17:40 +0800

    广告位管理

--- src/views/advert-space/index.vue
+++ src/views/advert-space/index.vue
@@ -1,10 +1,76 @@
 <template>
-  <div class="advert-space">广告位管理</div>
+  <div class="menu">
+    <el-card>
+      <div slot="header">
+        <el-button :disabled="isLoading">添加广告位</el-button>
+      </div>
+      <el-table
+      :data="menus"
+      border
+      style="width: 100%">
+        <el-table-column
+          prop="key"
+          label="spaceKey"
+          type="index"
+          width="150"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="广告位名称"
+          min-width="150"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="createTime"
+          label="创建时间"
+          min-width="150"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="updateTime"
+          label="更新时间"
+          min-width="150"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          label="操作"
+          min-width="150"
+          align="center">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.row)">编辑</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'AdvertSpaceIndex'
+  name: 'AdvertSpaceIndex',
+  data () {
+    return {
+      isLoading: false,
+      menus: []
+    }
+  },
+  created () {
+  },
+  methods: {
+    handleEdit (item) {
+      console.log(item)
+    },
+    handleDelete(item) {
+      console.log(item)
+    }
+  },
 }
 </script>
 
