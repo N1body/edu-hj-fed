e68eab7fce86985f589b7c7b3d78a0a7c538cf2d
N1body <hjing0524@gmail.com>
2022-05-06 20:17:31 +0800

    广告管理

--- src/views/advert/index.vue
+++ src/views/advert/index.vue
@@ -1,12 +1,88 @@
 <template>
-  <div class="advert">
-    广告管理
+  <div class="menu">
+    <el-card>
+      <div slot="header">
+        <el-button :disabled="isLoading">添加广告</el-button>
+      </div>
+      <el-table
+      :data="menus"
+      border
+      style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="id"
+          type="index"
+          width="70"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="广告名称"
+          min-width="150"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="position"
+          label="广告位置"
+          min-width="150"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="icon"
+          label="广告图片"
+          min-width="150"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="time"
+          label="时间"
+          min-width="150"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="status"
+          label="上线/下线"
+          min-width="150"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          label="操作"
+          min-width="150"
+          align="center">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.row)">编辑</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AdvertIndex'
+  name: 'AdvertIndex',
+  data () {
+    return {
+      isLoading: false,
+      menus: []
+    }
+  },
+  created () {
+  },
+  methods: {
+    handleEdit (item) {
+      console.log(item)
+    },
+    handleDelete(item) {
+      console.log(item)
+    }
+  },
 }
 </script>
 
