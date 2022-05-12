86031dac61750d431f4e8e5de2a34722356b1b7b
N1body <hjing0524@gmail.com>
2022-05-12 15:51:36 +0800

    课程管理组件

--- src/views/course/index.vue
+++ src/views/course/index.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="course">课程管理</div>
+  <!-- 课程管理首页 -->
+  <div class="course">
+    <CourseList/>
+  </div>
 </template>
 
 <script>
+import CourseList from './components/CourseList.vue'
 export default {
-  name: 'CourseIndex'
+  name: 'CourseIndex',
+  components: {
+    CourseList
+  }
 }
 </script>
 
