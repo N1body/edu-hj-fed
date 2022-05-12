e651df1799f1196acd871e8275fb4a7fc69ee32c
N1body <hjing0524@gmail.com>
2022-05-12 15:48:29 +0800

    头部栏组件

--- src/layout/components/app-header.vue
+++ src/layout/components/app-header.vue
@@ -1,9 +1,13 @@
 <template>
+  <!-- 头部栏部分 -->
   <div class="header">
+    <!-- 面包屑组件 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <!-- 通过获取meta属性中的title来获取现在所在的导航 -->
       <el-breadcrumb-item>{{routeRecord}}</el-breadcrumb-item>
     </el-breadcrumb>
+    <!-- 头像下拉部分 -->
     <el-dropdown @command="handleCommand">
       <span class="el-dropdown-link">
         <el-avatar :src="userInfo.portrait ||  require('../../assets/default-avatar.png')"></el-avatar>
@@ -26,15 +30,17 @@ export default {
     }
   },
   created () {
+    // 加载用户信息
     this.loadUserInfo()
   },
   methods: {
+    // 退出登录功能
     handleCommand(command) {
       if (command === 'exit') {
-          this.$confirm('确认退出吗?', '退出提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
+        this.$confirm('确认退出吗?', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
         }).then(() => {
           // 清楚用户的登录状态
           this.$store.commit('setUser', null)
@@ -56,6 +62,7 @@ export default {
         })
       }
     },
+    // 加载用户信息
     async loadUserInfo () {
       const { data } = await getUerInfo()
       if (data.state !== 1) {
