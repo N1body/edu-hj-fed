96c433f5d91705919ad10fd86ed30e21967caf07
N1body <hjing0524@gmail.com>
2022-05-12 15:51:19 +0800

    课程添加组件

---
+++ src/views/course/Create.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- 添加课程组件 -->
+  <div>
+    <CreateOrEdit/>
+  </div>
+</template>
+
+<script>
+import CreateOrEdit from './components/CreateOrEdit.vue'
+export default {
+  name: 'AddCourse',
+  components: {
+    CreateOrEdit
+  }
+}
+</script>
+
+<style>
+
+</style>
