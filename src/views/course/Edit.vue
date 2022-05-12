cb7e8a22547b69d30eaa139b701e2461fdb40745
N1body <hjing0524@gmail.com>
2022-05-12 15:51:27 +0800

    课程编辑组件

---
+++ src/views/course/Edit.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- 编辑课程组件 -->
+  <div>
+    <CreateOrEdit :isEdit="true" :courseId="courseId"/>
+  </div>
+</template>
+
+<script>
+import CreateOrEdit from './components/CreateOrEdit.vue'
+export default {
+  name: 'EditCoure',
+  props: ['courseId'],
+  components: {
+    CreateOrEdit
+  }
+}
+</script>
+
+<style>
+
+</style>
