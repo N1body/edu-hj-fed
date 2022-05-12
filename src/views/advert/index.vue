f8eb2b0e9eeeee46be452cff5f0146737ec5edcf
N1body <hjing0524@gmail.com>
2022-05-12 15:50:49 +0800

    广告管理组件

--- src/views/advert/index.vue
+++ src/views/advert/index.vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- 广告列表组件 -->
+  <!-- 由于接口有问题,所以只做了各基本框架 -->
   <div class="menu">
     <el-card>
       <div slot="header">
@@ -74,6 +76,7 @@ export default {
     }
   },
   created () {
+    this.$message.info('此功能待开发')
   },
   methods: {
     handleEdit (item) {
